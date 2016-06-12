279cc6ad4034c628d7b5d1d6dd143a77f8a694e3
赵明宇 <498973030@qq.com>
2016-06-12 15:28:45 +0800

    添加头文件，.gitigonre过滤数据集避免其加入仓库

--- src/headers.h
+++ src/headers.h
@@ -1,24 +1,3 @@
-// -----------------------------------------------------------------------------
-// Copyright (c) 2013-2018 Sun Yat-Sen University (SYSU). All Rights Reserved.
-//
-// SYSU grants permission to use, copy, modify, and distribute this software
-// and its documentation for NON-COMMERCIAL purposes and without fee, provided 
-// that this copyright notice appears in all copies.
-//
-// SYSU provides this software "as is," without representations or warranties
-// of any kind, either expressed or implied, including but not limited to the
-// implied warranties of merchantability, fitness for a particular purpose, 
-// and noninfringement. SYSU shall not be liable for any damages arising from
-// any use of this software.
-//
-// Authors: Qiang Huang  (huangq2011@gmail.com)
-//          Jianlin Feng (fengjlin@mail.sysu.edu.cn)
-
-//
-// Created on:       2014-03-12
-// Last Modified on: 2016-04-01
-// Version 1.1.2
-// -----------------------------------------------------------------------------
 #include <stdio.h>
 #include <assert.h>
 #include <errno.h>
@@ -45,6 +24,7 @@
 #include "b_node.h"
 #include "b_tree.h"
 #include "LItem.h"
+#include "BoxMuller.h"
 #include "medrank.h"
 
 using namespace std;
